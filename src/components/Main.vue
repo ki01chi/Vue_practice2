7209694c4067aa03ed1d903494572eed648a3081
chisato <kitada.c08@gmail.com>
2020-10-22 17:04:43 +0900

    app.vueを修正

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="main-container">
+    <TextBox />
+    <div class="devider"></div>
+    mainが入るよ
+  </main>
+</template>
+
+<script>
+import TextBox from './TextBox';
+
+export default {
+  components: {
+    TextBox,
+  }
+}
+</script>
+
+<style scoped>
+.main-container {
+  left: 200px;
+  width: calc(100% - 200px);
+  flex: 1;
+  position: absolute;
+}
+
+.devider {
+  border-top: 10px solid #ccc;
+}
+</style>
